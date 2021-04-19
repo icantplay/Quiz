9898d5818165202c5a277b610f21aacb215e067c
Gonçalo Martins <45426228+icantplay@users.noreply.github.com>
2021-04-19 15:54:21 +0100

    Add files via upload

---
+++ Quizz/question_dinossauro.js
@@ -0,0 +1,21 @@
+
+const quiz = [
+    {
+        q: 'Alguns Dinossauros eram muito rápidos. Sabes qual era o Dinossauro que corria mais rápido?',
+        options: ['Velociraptor','Estrutiomimo','Tiranossauro Rex'],
+        answer: 1
+
+
+    },
+
+    {
+        q: 'O maior Dinossauro podia chegar aos 50 metros. Sabes qual era o maior Dinossauro?',
+        options: ['Sismossauro','Actiossauros','Coritossauro '],
+        answer: 1
+
+
+    },
+   
+
+
+]
